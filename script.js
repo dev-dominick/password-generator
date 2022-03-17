027a9d5cb36fafdd4e8089d1c23ad46f61b8e3fa
Dom <Domalbano35@gmail.com>
2022-03-17 17:56:26 -0400

    logic changes

--- script.js
+++ script.js
@@ -11,11 +11,15 @@ var lettersLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", 
 var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 // Var of Special characters
-var character = ["!", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "`", "{", "|", "}", "~" ];
+var character = ["!", '"', "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "`", "{", "|", "}", "~"];
+
+//set state
+let inRange = false
 
 
 
 //generatepass gets stored in password 
+
 
 
 // Write password to the #password input
@@ -30,78 +34,95 @@ function writePassword() {
 // Added an event of listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+// function generatePassword() {
+//   var tempPassword = [];
+
+//   //to get random 
+
+
+
+
+
+//   // Math.random() * userPass.length
+
+
+//   var finalPassword = "";
+
+
+
+//   return finalPassword;
+// }
+
+
+// var specialCharChoice = window.confirm(
+//   "Do you want special characters in your password"
+// )
+
+
+
+
+
+
 function generatePassword() {
-  var finalPassword = "hello";
-
-
-
-  return finalPassword;
-}
-
-
-
-
-
-function generatePassword() { 
   var userPassword = Math.ceil(
-    prompt("Choose a password length between 8 and 128 characters.")
-  ); 
+    prompt("Choose a password length between 8 and 128 characters."))
   console.log(userPassword);
+
 
   if (isNaN(userPassword)) {
     prompt("Please input a number between 8 and 128 in this field.")
+    generatePassword();
+
   }
 
-  else {
-    generatePassword()
+  // if (userPassword >= 8 && userPassword <= 128) {
+  //     generatePassword();
+  // } 
 
+
+  // Condition if user inputs password length less than 8 characters.
+  if (userPassword < 8 || userPassword > 128) {
+    confirm(
+      "Password needs to be at least 8 characters and no more than 128."
+    );
     return;
   }
-  
-//todo ask about syntax and repeat nan prompts
-
-  //why did the syntax require a curly before the declaration
-
-  
-
-
-  if (userPassword >= 8 && userPassword <=128) {
-    console.log(userPass);
-    
-  } else {
-    prompt("Password need to be between 8 and 128 character.");
-    
-    return;
+  // // Condition if user inputs password length more than 128 characters.
+  if (userPassword > 8 || userPassword < 128) {
+    console.log(userPassword);
   }
- 
-//Condition if user inputs password length less than 8 characters.
-  if (userPassword < 8) {
-    prompt("Password needs to be at least 8 characters. Please select a password length between 8 and 128 characters.")
-  }
-// Condition if user inputs password length more than 128 characters.
-  if (userPassword > 128) {
-    prompt("Password needs to be less than 128 characters. Please select a password length between 8 and 128 characters.")
-  }
-    var passUpper = confirm("Press OK if you want to include capital letters.")
-        console.log(passUpper);
-    
-    var passLower = confirm("Press OK if you want to include lower case letters.");
-        console.log(passLower);
 
-    var passNumber = confirm("Press OK if you want to include numbers.");
-        console.log(passNumber);
+  var passUpper = confirm("Press OK if you want to include capital letters.")
+  console.log(passUpper);
 
-    var passCharacter = confirm("Press OK if you want to include special characters.");
-        console.log(passCharacter);
-  
+  var passLower = confirm("Press OK if you want to include lower case letters.");
+  console.log(passLower);
+
+  var passNumber = confirm("Press OK if you want to include numbers.");
+  console.log(passNumber);
+
+  var passCharacter = confirm("Press OK if you want to include special characters.");
+  console.log(passCharacter);
+
+
   if (passUpper === false && passLower === false && passNumber === false && passCharacter === false) {
     alert("You must select yes to at least one of the conditions")
+    generatePassword()
   }
-  
-  
 
+  return userPassword;
+
+
+
+
+
+
+
+
+
+  
 }
 
 
 
-//
+
